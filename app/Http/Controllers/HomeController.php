29cf2918105589dd665a107aaaec88e3181384e8
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-04-04 14:17:25 +0200

    Refactor index method in HomeController; update date handling and remove commented-out code for winner number creation

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,18 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\WinnerNumber;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $winner_numbers = $this->generateNumbers();
-        WinnerNumber::create([
-            'numbers' => implode(',', $winner_numbers),
-        ]);
+        // WinnerNumber::create([
+        //     'numbers' => implode(',', $winner_numbers),
+        // ]);
         $dash = [];
-        $string = WinnerNumber::all()->pluck('numbers')->toArray();
-        foreach ($string as $key => $value) {
+        $rows = WinnerNumber::all()->pluck('numbers')->toArray();
+$date_ = WinnerNumber::orderBy('created_at', 'desc')->first();
+$date = Carbon::parse($date_->created_at);
+$last_date=$date->format('Y-m-d');
+$now = Carbon::now();
+$last_week = $now->subDays(7)->format('Y-m-d');
+
+echo ($last_week<$last_date)?"nem kell sorsolni":"folytatni kell";
+dd($last_week);
+        foreach ($rows as $key => $value) {
             $nums = explode(',', $value);
             foreach ($nums as $num) {
                 if (isset($dash[$num])) {
