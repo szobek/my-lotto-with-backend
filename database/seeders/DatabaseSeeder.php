488dc9fc953c843c7d510090a0691b6eba06d590
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-04-12 18:17:46 +0200

    Add Role and UserRole models, factories, and migration; seed roles and user roles in the database.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Balance;
+use App\Models\Role;
 use App\Models\Ticket;
 use App\Models\User;
+use App\Models\UserRole;
 use App\Models\WinnerNumber;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -29,5 +31,7 @@ class DatabaseSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now(),
         ]);
+        Role::factory(20)->create();
+        UserRole::factory(10)->create();
     }
 }
